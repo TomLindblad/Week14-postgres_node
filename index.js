6972313dda025a79e20e378e3f051edd02665106
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2025-04-29 09:48:13 +0200

    started server

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const { Pool } = pg;
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+app.get("/", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM players");
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.listen(3000, (req, res) => {
+    console.log("Server is up and runnin' on port: 3000!")
+})
