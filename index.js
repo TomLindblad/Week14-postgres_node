c89c0923f7314e94aff2ef0e623d257a593532c8
TomLindblad <168995337+TomLindblad@users.noreply.github.com>
2025-04-29 10:47:49 +0200

    added all endpoints

--- index.js
+++ index.js
@@ -17,7 +17,70 @@ const pool = new Pool({
 
 app.get("/players_scores", async (req, res) => {
     try {
-        const result = await pool.query("SELECT players.name, games.title, scores.score FROM scores JOIN games ON games.id = game_id JOIN players ON players.id = player_id")
+        const result = await pool.query(`
+            SELECT players.name, games.title, scores.score 
+            FROM scores 
+            JOIN games ON games.id = game_id 
+            JOIN players ON players.id = player_id
+            `)
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/top_players", async (req, res) => {
+    try {
+        const result = await pool.query(`
+            SELECT players.name, SUM(scores.score) AS highscore 
+            FROM players 
+            JOIN scores ON players.id = scores.player_id 
+            GROUP BY players.name 
+            ORDER BY highscore DESC 
+            LIMIT 3
+            `)
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.get("/inactive_players", async (req, res) => {
+    try {
+        const result = await pool.query(`
+            SELECT players.name
+            FROM players
+            LEFT OUTER JOIN scores ON players.id = player_id
+            WHERE scores.score IS NULL
+            `)
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+
+app.get("/popular_genres", async (req, res) => {
+    try {
+        const result = await pool.query(`
+            SELECT games.genre, COUNT(scores.game_id) AS times_played  
+            FROM scores 
+            JOIN games ON scores.game_id = games.id 
+            GROUP BY games.genre
+            ORDER BY times_played DESC
+            `)
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+app.get("/recent_players", async (req, res) => {
+    try { 
+        const result = await pool.query(`
+            SELECT name, join_date
+            FROM players
+            WHERE join_date > CURRENT_TIMESTAMP - INTERVAL '30 DAY'
+            `)
         res.json(result.rows);
     } catch (error) {
         res.status(500).send(error);
